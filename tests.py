96f91da9c951c210c5ff76c0dd28d41c88d778f9
elraro <elraro@elraro.eu>
2016-10-03 12:50:23 +0200

    Querys 2

--- tests.py
+++ tests.py
@@ -7,15 +7,20 @@ DB_PASS = "VXxL4UOLvB6wc01Y3Cxi"
 DB_NAME = "frav_ABC"
 
 con = None
+
+con = mdb.connect(DB_HOST, DB_USER, DB_PASS, DB_NAME)
+cur = con.cursor()
+
 try:
-    con = mdb.connect(DB_HOST, DB_USER, DB_PASS, DB_NAME)
-    cur = con.cursor()
-    cur.execute("SELECT VERSION()")
-    ver = cur.fetchone()
-    print("Database version : %s " % ver)
-except mdb.Error as e:
-    print("Error %d: %s" % (e.args[0], e.args[1]))
-    sys.exit(1)
-finally:
-    if con:
-        con.close()
+    cur.execute(
+        "INSERT INTO " + DB_NAME + " VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,\
+         %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,\
+         %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,\
+         %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+        (188, 90))
+    con.commit()
+except:
+    print("error")
+    con.rollback()
+
+con.close()
