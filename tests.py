3fcd636260ff2bb2a1a563e6a8ec354f56fbbd10
elraro <elraro@elraro.eu>
2016-10-06 12:31:27 +0200

    tests2

--- tests.py
+++ tests.py
@@ -6,21 +6,26 @@ DB_USER = "frav"
 DB_PASS = "VXxL4UOLvB6wc01Y3Cxi"
 DB_NAME = "frav_ABC"
 
-con = None
+# con = None
+#
+# con = mdb.connect(DB_HOST, DB_USER, DB_PASS, DB_NAME)
+# cur = con.cursor()
+#
+# try:
+#     cur.execute(
+#         "INSERT INTO " + DB_NAME + " VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,\
+#          %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,\
+#          %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,\
+#          %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+#         (188, 90))
+#     con.commit()
+# except:
+#     print("error")
+#     con.rollback()
+#
+# con.close()
 
-con = mdb.connect(DB_HOST, DB_USER, DB_PASS, DB_NAME)
-cur = con.cursor()
+a = "D:\SIBAR\FRAV_ABC\Logitech\fluorescente\Logitech_flu_009\Frames\Camera MEDIUM\frame_Logitech_flu_009_MED_014.jpg".replace("\f", "\\f")
+a = a.split("\\")
+print(a)
 
-try:
-    cur.execute(
-        "INSERT INTO " + DB_NAME + " VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,\
-         %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,\
-         %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,\
-         %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-        (188, 90))
-    con.commit()
-except:
-    print("error")
-    con.rollback()
-
-con.close()
