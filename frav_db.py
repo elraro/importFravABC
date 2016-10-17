0d98a3262ab75c2dfc4d8206981b9264d22b3cc4
elraro <elraro@elraro.eu>
2016-10-17 11:59:50 +0200

    Añadida variable de carpeta

--- frav_db.py
+++ frav_db.py
@@ -6,17 +6,18 @@ DB_HOST = "localhost"
 DB_USER = "frav"
 DB_PASS = "VXxL4UOLvB6wc01Y3Cxi"
 DB_NAME = "frav_ABC"
+FOLDER = "/media/alberto/Datos/FRAV_ALBERTO/"
 
 con = Mdb.connect(DB_HOST, DB_USER, DB_PASS, DB_NAME)
 cur = con.cursor()
 
-cur.execute(querys.create_camera_table())
-cur.execute(querys.create_light_table())
-cur.execute(querys.insert_camera_data())
-cur.execute(querys.insert_light_data())
-cur.execute(querys.create_data_table("fir_data"))
-cur.execute(querys.create_data_table("imgs_data"))
-con.commit()
+# cur.execute(querys.create_camera_table())
+# cur.execute(querys.create_light_table())
+# cur.execute(querys.insert_camera_data())
+# cur.execute(querys.insert_light_data())
+# cur.execute(querys.create_data_table("fir_data"))
+# cur.execute(querys.create_data_table("imgs_data"))
+# con.commit()
 
 
 def insert_data(file, table, connection, cursor, symbol=","):
@@ -39,33 +40,36 @@ def insert_data(file, table, connection, cursor, symbol=","):
 
 
 # fir
-insert_data("/media/alberto/Datos/FRAV_ALBERTO/FIR/DATA/logitech_fluorescente_data.txt",
-            "fir_data", con, cur)
-insert_data("/media/alberto/Datos/FRAV_ALBERTO/FIR/DATA/logitech_halogeno_data.txt", "fir_data", con,
-            cur)
-insert_data("/media/alberto/Datos/FRAV_ALBERTO/FIR/DATA/logitech_led_data.txt", "fir_data", con, cur)
-insert_data("/media/alberto/Datos/FRAV_ALBERTO/FIR/DATA/logitech_nir_data.txt", "fir_data", con, cur)
-insert_data("/media/alberto/Datos/FRAV_ALBERTO/FIR/DATA/microsoft_fluorescente_data.txt",
-            "fir_data", con, cur)
-insert_data("/media/alberto/Datos/FRAV_ALBERTO/FIR/DATA/microsoft_halogeno_data.txt", "fir_data",
-            con, cur)
-insert_data("/media/alberto/Datos/FRAV_ALBERTO/FIR/DATA/microsoft_led_data.txt", "fir_data", con, cur)
+# insert_data(FOLDER + "FIR/DATA/logitech_fluorescente_data.txt",
+#             "fir_data", con, cur)
+# insert_data(FOLDER + "FIR/DATA/logitech_halogeno_data.txt", "fir_data", con,
+#             cur)
+# insert_data(FOLDER + "FIR/DATA/logitech_led_data.txt", "fir_data", con, cur)
+# insert_data(FOLDER + "FIR/DATA/logitech_nir_data.txt", "fir_data", con, cur)
+# insert_data(FOLDER + "FIR/DATA/microsoft_fluorescente_data.txt",
+#             "fir_data", con, cur)
+# insert_data(FOLDER + "FIR/DATA/microsoft_halogeno_data.txt", "fir_data",
+#             con, cur)
+# insert_data(FOLDER + "FIR/DATA/microsoft_led_data.txt", "fir_data", con, cur)
 
 # img data
-# insert_data("/media/alberto/Datos/FRAV_ALBERTO/DATA_INFO/DATA/logitech_fluorescente_imgs_data_CORREGIDO.csv",
-#             "imgs_logitech_fluorescente_data", con, cur, ";")
-# insert_data("/media/alberto/Datos/FRAV_ALBERTO/DATA_INFO/DATA/logitech_halogeno_imgs_data_CORREGIDO.csv",
-#             "imgs_logitech_halogeno_data", con, cur, ";")
-# insert_data("/media/alberto/Datos/FRAV_ALBERTO/DATA_INFO/DATA/logitech_led_imgs_data_CORREGIDO.csv",
-#             "imgs_logitech_led_data", con, cur, ";")
-# insert_data("/media/alberto/Datos/FRAV_ALBERTO/DATA_INFO/DATA/logitech_nir_imgs_data_CORREGIDO.csv",
-#             "imgs_logitech_nir_data", con, cur, ";")
-# insert_data("/media/alberto/Datos/FRAV_ALBERTO/DATA_INFO/DATA/microsoft_fluorescente_imgs_data_CORREGIDO.csv",
-#             "imgs_microsoft_fluorescente_data", con, cur, ";")
-# insert_data("/media/alberto/Datos/FRAV_ALBERTO/DATA_INFO/DATA/microsoft_halogeno_imgs_data_CORREGIDO.csv",
-#             "imgs_microsoft_halogeno_data", con, cur, ";")
-# insert_data("/media/alberto/Datos/FRAV_ALBERTO/DATA_INFO/DATA/microsoft_led_imgs_data_CORREGIDO.csv",
-#             "imgs_microsoft_led_data", con, cur, ";")
+insert_data(FOLDER + "DATA_INFO/DATA/logitech_fluorescente_imgs_data.csv",
+            "imgs_data", con, cur, ";")
+insert_data(FOLDER + "DATA_INFO/DATA/logitech_halogeno_imgs_data.csv",
+            "imgs_data", con, cur, ";")
+insert_data(FOLDER + "DATA_INFO/DATA/logitech_led_imgs_data.csv",
+            "imgs_data", con, cur, ";")
+insert_data(FOLDER + "DATA_INFO/DATA/logitech_nir_imgs_data.csv",
+            "imgs_data", con, cur, ";")
+insert_data(FOLDER + "DATA_INFO/DATA/microsoft_fluorescente_imgs_data.csv",
+            "imgs_data", con, cur, ";")
+insert_data(FOLDER + "DATA_INFO/DATA/microsoft_halogeno_imgs_data.csv",
+            "imgs_data", con, cur, ";")
+insert_data(FOLDER + "DATA_INFO/DATA/microsoft_led_imgs_data.csv",
+            "imgs_data", con, cur, ";")
+
+#scores
+# TODO
 
 # close con
 con.close()
